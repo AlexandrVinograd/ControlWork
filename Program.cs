584ad3e1f6c223e53b7b2ac2c1e12badb9742a2f
AlexandrVinogradov <vanhorn.av@gmail.com>
2024-03-09 20:41:25 +0300

    Add means FilterStrings

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
 ﻿
+string[] FilterStrings(string[] originalArray)
+{
+    int count = 0;
+    // Подсчет количества строк, длина которых не превышает 3 символов
+    foreach (var item in originalArray)
+    {
+        if (item.Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    // Создание нового массива с подходящими строками
+    string[] newArray = new string[count];
+    int index = 0;
+    foreach (var item in originalArray)
+    {
+        if (item.Length <= 3)
+        {
+            newArray[index++] = item;
+        }
+    }
+
+    return newArray;
+}
+
+
